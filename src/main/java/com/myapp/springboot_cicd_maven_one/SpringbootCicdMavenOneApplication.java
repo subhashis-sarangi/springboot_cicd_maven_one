f3ef5f478042ed3c1256e6af205a9ba95c2604e3
subhashis <subhashis.sarangi@gmail.com>
2024-03-30 10:04:59 +0530

    commit

---
+++ src/main/java/com/myapp/springboot_cicd_maven_one/SpringbootCicdMavenOneApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.springboot_cicd_maven_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdMavenOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCicdMavenOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/springboot_cicd_maven_one/SpringbootCicdMavenOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springboot_cicd_maven_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicdMavenOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
